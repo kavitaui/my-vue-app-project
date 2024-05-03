86cff7807f536875f582f036a7d4c8a7c94cdc12
kavitaui <122557323+kavitaui@users.noreply.github.com>
2024-05-03 10:22:50 +0100

    check commit

--- src/Component.tsx
+++ src/Component.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Component = () => {
     return (
         <div>Component hello kavita
-            hey manoj
+            hey manoj and kavita
         </div>
     )
 }
